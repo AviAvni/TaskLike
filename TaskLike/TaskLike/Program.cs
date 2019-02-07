cd3acd3e9ee3a84a4d617c39aa6b3205fc77ca3d
Dima Abramchaev <DimaA@call.local>
2019-02-07 16:29:55 +0200

    Cleaned up some code in AsyncTaskLikeMethodBuilder

--- TaskLike/TaskLike/Program.cs
+++ TaskLike/TaskLike/Program.cs
@@ -9,27 +9,32 @@ namespace TaskLike
         static async Task Main(string[] args)
         {
             var list = Enumerable.Range(0, 10);
-            var res = await DoSomething(new ListLike<int>(list), new ListLike<int>(list));
+            var res = await DoSomething(new ListLike<int>(list), new ListLike<int>(list), new ListLike<int>(list));
 
             var q = (from i in Enumerable.Range(0, 10)
                      from j in Enumerable.Range(0, 10)
-                     select i * 10 + j).ToList();
+                     from k in Enumerable.Range(0, 10)
+                     select i * 100 + j * 10 + k).ToList();
 
             Console.WriteLine(res.SequenceEqual(q));
-            //while (!task.IsCompleted)
-            //{
-            //    Console.WriteLine(string.Join(", ", task._result));
-            //    await Task.Delay(10000);
-            //}
         }
 
-        static async TaskLike<int> DoSomething(ListLike<int> list, ListLike<int> list1)
+        static async TaskLike<int> DoSomething(ListLike<int> list, ListLike<int> list1, ListLike<int> list2)
         {
             var i = await list;
+            var s1 = i;
             var j = await list1;
-            //await Task.Delay(1000);
-            Console.WriteLine($"{i}, {j}");
-            return i * 10 + j;
+            var s2 = s1 * 10 + j;
+            var k = await list2;
+            var res = s2 * 10 + k;
+
+            await Task.Delay(100);
+
+            var t = await Task.FromResult(res);
+
+            Console.WriteLine($"{i}{j}{k}, {t}");
+
+            return res;
         }
     }
 }
